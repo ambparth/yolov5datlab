4f5b6872b3a934b785d6c4dfb5dcba0dc47f4baf
Ambarish PARTHASARATHY <45915770+ambparth@users.noreply.github.com>
2022-10-28 15:56:24 +0530

    Detect the objects and copy it to frames folder.

--- detect_comb.py
+++ detect_comb.py
@@ -1,4 +1,20 @@
-!cd ./that file name in the yolov5
+#%% importing the modules
+import os
+import shutil
 
-!python <PATH OF THE detect.py file>  --data <PATH/OF/FRAMES/> --conf-thres <THRESH> --save-txt 
 
+!cd <DIRECTORY POINTED TO YOLOv5 FOLDER>
+
+#%% detection of the objects in the frame. 
+!python <PATH OF THE detect.py file>  --weights yolov5s.pt --source <PATH/OF/FRAMES/> --conf-thres <THRESH> --save-txt 
+
+#%% saving the txt files and copying it into the frames directory. 
+
+txt_files = "/PATH/OF/TXT-FILES/"
+frames_files = "/PATH/OF/FRAMES/"
+
+files = os.listdir(txt_files)
+
+# Fetching all the files to directory
+for file_name in files:
+   shutil.copy(txt_files+file_name, frames_files+file_name)
