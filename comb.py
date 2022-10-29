05b762e34f3cc4c9c29f9be9a0e81b942f3fb6eb
Ambarish PARTHASARATHY <45915770+ambparth@users.noreply.github.com>
2022-10-29 11:10:53 +0530

    Update comb.py

--- comb.py
+++ comb.py
@@ -8,7 +8,7 @@ txt_files = "/PATH/OF/TXT-FILES/"
 frames_files = "/PATH/OF/FRAMES/"
 
 files = os.listdir(txt_files)
-
+os.mkdir('</SEPERATE/PATH>')
 # Fetching all the files to directory
 for file_name in files:
    shutil.copy(txt_files+file_name, frames_files+file_name)
