89226c3fab26841f6e2907e9fe4bd8841add436b
Ambarish PARTHASARATHY <45915770+ambparth@users.noreply.github.com>
2022-10-30 12:26:39 +0530

    Image and Label (.txt) combination

--- comb.py
+++ comb.py
@@ -8,7 +8,13 @@ txt_files = "/PATH/OF/TXT-FILES/"
 frames_files = "/PATH/OF/FRAMES/"
 
 files = os.listdir(txt_files)
+files_frame = os.listdir(frames_file)
 os.mkdir('</SEPERATE/PATH>')
+seprt_file = "/SEPERATE/PATH"
 # Fetching all the files to directory
 for file_name in files:
-   shutil.copy(txt_files+file_name, frames_files+file_name)
+   shutil.copy(txt_files+file_name, seprt_file+file_name)
+print('Done copying txt files');
+for f1 in files_frame:
+   shutil.copy(frames_files+f1, seprt_file+f1)
+print('Done copying the frames');
