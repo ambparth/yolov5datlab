0aa4fef81b305c4903c2f50bf85a0aedb800dae2
Ambarish PARTHASARATHY <45915770+ambparth@users.noreply.github.com>
2023-05-26 10:56:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ import os
 import cv2
 
 #%% PART 1: PARTITION INTO FRAMES
-os.mkdir('/SAVED/PATH/')
-KPS = 1
-VIDEO_PATH = "/VIDEO/PATH/" 
-IMAGE_PATH = "/IMAGE/PATH/" 
-EXTENSION = ".jpg" 
-cap = cv2.VideoCapture(VIDEO_PATH) 
+os.mkdir('/SAVED/PATH/') # create the folder 
+KPS = 1 # target FPS rate is given here 
+VIDEO_PATH = "/VIDEO/PATH/" # path of the video file 
+IMAGE_PATH = "/IMAGE/PATH/"  # path where the images are to be saved
+EXTENSION = ".jpg" # extension in which the images are to be saved. 
+cap = cv2.VideoCapture(VIDEO_PATH)  
 fps = round(cap.get(cv2.CAP_PROP_FPS))
-print(fps)
+print(fps) # print the fps rate. 
 # exit()
 hop = round(fps / KPS) 
 curr_frame = 0 
-i = 0;
+i = 0; # temporatory variable 
 while(True): 
-    ret, frame = cap.read()
-    frame = frame[0:1080,0:1920] 
+    ret, frame = cap.read() # read the frame. 
+    frame = frame[0:1080,0:1920] # crop the region 1080x1920 
     #print(curr_frame)
-    if not ret: break 
-    if curr_frame % hop == 0: 
-        print([curr_frame,i]); 
-        name = IMAGE_PATH + "/" + str(i) + EXTENSION 
-        cv2.imwrite(name, frame) 
-        i+=1
-    curr_frame += 1  
-cap.release() 
+    if not ret: break # if there is no frame then break the loop
+    if curr_frame % hop == 0: # condition
+        print([curr_frame,i]); # for verification if the route is correct or not. 
+        name = IMAGE_PATH + "/" + str(i) + EXTENSION # note the path 
+        cv2.imwrite(name, frame) # write the image and to the directory specified
+        i+=1 # increment the count
+    curr_frame += 1  #increment the frame
+cap.release() # once the reading is done, release or come out of the loop. 
 
 #%% PART2; TRANSFER LEARNING
 
